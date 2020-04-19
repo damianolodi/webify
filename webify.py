1d722fc3e6c7fd11933e8ac80d9573a29d0e51ec
Damiano <damiano.lodi.93@gmail.com>
2020-04-19 08:54:45 +0200

    Complete the module structure.
    The first version of the CLI will have 4 functions:
    - resize the image mainitaining the proportion
    - convert png to jpg
    - convert jpg to png when background removal is requested
    - remove (white) background of the image
    
    Each of this functions has a complementary Python function that is
    called by "main" based on the options passed to the CLI.

---
+++ webify.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import typer
+
+
+def resize_image():
+    pass
+
+
+def remove_bakground():
+    pass
+
+
+def save_jpg():
+    pass
+
+
+def save_png():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    typer.run(main)
